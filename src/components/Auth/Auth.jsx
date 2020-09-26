022597355cf2652107612bd862162fd9bb4e029a
Eugene <tplrt901@gmail.com>
2020-09-26 14:24:44 +0300

    added error handling to auth page

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,27 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 import style from "./Auth.module.css"
 import {FloatingWrapper} from "../FloatingWrapper/FloatingWrapper";
+import {useHttp} from "../../hooks/http.hook";
 
 export const Auth = () => {
+    const {loading, error, request, clearError} = useHttp()
+    const [form, setForm] = useState({email: '', password: ''})
+
+    const changeHandler = (event) => {
+        setForm({...form, [event.target.name]: event.target.value})
+        clearError()
+    }
+
+    const loginHandler = async () => {
+        try {
+            const data = await request('/api/auth/login', 'POST', {...form})
+
+        } catch (e) {
+        } finally {
+            setForm({email: '', password: ''})
+        }
+    }
 
     return (
         <FloatingWrapper>
             <div className={style.wrapper}>
                 <h1 className={style.header}>Login</h1>
                 <form action="" className={style.form} autoComplete={'off'}>
+                    <span className={`${style.error} ${error ? '' : style.errorHidden}`}>{error ?? ''}</span>
                     <div className={style.formItem}>
-                        <input type="text" className={style.input} name={'email'} required={true} />
+                        <input type="text" value={form.email} className={style.input} name={'email'} required={true}
+                               onChange={changeHandler}/>
                         <label className={style.label}>
                             <span className={style.labelContent}>Email</span>
                         </label>
                     </div>
                     <div className={style.formItem}>
-                        <input type="password" className={style.input} required={true}/>
+                        <input type="password" value={form.password} name={'password'} className={style.input}
+                               required={true} onChange={changeHandler}/>
                         <label className={style.label}>
                             <span className={style.labelContent}>Password</span>
                         </label>
                     </div>
-                    <button className={style.button}>Login</button>
+                    <button className={style.button} onClick={loginHandler} disabled={loading}>Login</button>
                 </form>
             </div>
         </FloatingWrapper>
